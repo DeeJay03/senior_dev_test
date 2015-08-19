de29a4d2912a4981192db928e0a78a98a9e7c112
DeeJay03 <djantoinette20@gmail.com>
2015-08-19 10:11:49 +1000

    Fix indentation

--- class/Article.php
+++ class/Article.php
@@ -1,47 +1,49 @@
 <?php
 
-	class Article {
-		private $article_id;
-		private $article_date;
-		private $headline;
-		private $intro;
-		private $story;
-		private $state;
+  class Article {
+    private $article_id;
+    private $article_date;
+    private $headline;
+    private $intro;
+    private $story;
+    private $state;
 		
-		function __construct($article_id, $date, $headline, $intro, $story, $state) {
-			$this->article_id = $article_id;
-			$this->article_date = $date;
-			$this->headline = $headline;
-			$this->intro = $intro;
-			$this->story = $story;
-			$this->state = $state;
-		}
+    function __construct($article_id, $date, $headline, $intro, $story, $state) {
+      $this->article_id = $article_id;
+      $this->article_date = $date;
+      $this->headline = $headline;
+      $this->intro = $intro;
+      $this->story = $story;
+      $this->state = $state;
+    }
 		
-		public function getArticleId() {
-			return $this->article_id;
-		}
+    public function getArticleId() {
+    	return $this->article_id;
+    }
 		
-		public function getDate() {
-			return $this->article_date;
-		}
+    public function getDate() {
+    	return $this->article_date;
+    }
 		
-		public function getHeadline() {
-			return $this->headline;
-		}
+    public function getHeadline() {
+    	return $this->headline;
+    }
 		
-		public function getIntro() {
-			return $this->intro;
-		}
+    public function getIntro() {
+    	return $this->intro;
+    }
 		
-		public function getStory() {
-			return $this->story;
-		}
+    public function getStory() {
+    	return $this->story;
+    }
 		
-		public function getState() {
-			return $this->state;
-		}
+    public function getState() {
+    	return $this->state;
+    }
 		
-		public function getArticle() {
-			return array('date' => $this->article_date, 'headline' => $this->headline, 'intro' => $this->intro, 'stroy' => $this->story);
-		}
-	}
+    public function getArticle() {
+    	return array('date' => $this->article_date, 'headline' => $this->headline, 'intro' => $this->intro, 'stroy' => $this->story);
+    }
+  }
+  
+?>
